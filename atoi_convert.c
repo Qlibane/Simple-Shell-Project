3bd1b402713de090496a756664a2bb7ba066d5ca
qlibane <qjirmo@gmail.com>
2023-12-12 23:48:00 -0800

    atoi_convert.c

---
+++ atoi_convert.c
@@ -0,0 +1,82 @@
+#include "shell.h"
+
+/**
+ * interactive - returns true if shell is in interactive mode
+ *
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim -  checks for delimeter
+ *
+ * @c: the char to check
+ *
+ * @delim: the string for delimeter
+ *
+ * Return: 1 if delimeter, 0 if not
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ *_isalpha - checks for an alphabetic character
+
+ *@c: The character to check
+
+ *Return: 1 if c is alphabetic, 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_atoi - converts a string to integer
+
+ *@s: the string to convert
+
+ *Return: 0 if no numbers in string, converted number if otherwise
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
